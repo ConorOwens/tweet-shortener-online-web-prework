fea745717468e16a96e3abcf6643ec9b64be3c74
Conor Owens <conorj.owens@gmail.com>
2019-04-11 03:31:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,7 +13,9 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweets)
-  
+  tweets.map do |tweet|
+    puts word_substituter(tweet)
+  end
 end
 
 def selective_tweet_shortener(tweet)
